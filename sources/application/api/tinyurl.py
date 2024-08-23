8b2c5653b2458c50f19f43c647293342a037e8f7
just3mhz <just3mhz@backend-dev.ru-central1.internal>
2024-08-23 18:22:11 +0000

    Use prefix

--- sources/application/api/tinyurl.py
+++ sources/application/api/tinyurl.py
@@ -5,16 +5,16 @@ from application.api.models import LongUrl
 from application.storage import storage
 from application.storage import TokenNotFoundError
 
-router = APIRouter()
+router = APIRouter(prefix='/api/v1/short_urls')
 
 
-@router.post('/api/v1/short_urls', status_code=201)
+@router.post('/', status_code=201)
 async def store(payload: LongUrl):
     token = await storage.store(url=payload.long_url)
     return {"token": token}
 
 
-@router.get('/api/v1/short_urls/{token}')
+@router.get('/{token}')
 async def retrieve(token: str):
     try:
         url = await storage.retrieve(token=token)
